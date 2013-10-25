e6c67492e7ace4c0a404e5a10f89d6e8694b2cd7
hoan <hoan@bluepego.com>
2013-10-24 23:49:38 -0700

    update song cataloging function

--- app/models/Songs.php
+++ app/models/Songs.php
@@ -7,34 +7,52 @@ class Songs extends Eloquent {
 	public static $rules = array();
 
 
-	public static function classifySongMedia( $id ){
-		$mediaEntries = Songs::getSongContents( $id );
-		foreach ( $mediaEntries as $entry ) {
-			$blowup = explode( '.', $entry );
-			$entryCount = count( $blowup );
-			if( $entryCount < 3 ){
-				echo $entry . ": unknown type \n";
+	public static function classifySongMedia(){
+		foreach( Songs::all() as $song ) {
+			echo "............ \n";
+			// echo "SONG: " . $song->title . "\n";
+			$mediaEntries = Songs::getSongContents( $song );
+
+			if( null != $mediaEntries ){
+				foreach ( $mediaEntries as $entry ) {
+					// echo "ENTRY: " . $entry . "\n";
+					$blowup = explode( '.', $entry );
+					$entryCount = count( $blowup );
+					if( 0 == strcmp( $entry, '.DS_Store' )) {
+						// skip
+					}
+					else if( $entryCount < 3 ){
+						echo $entry . ": unknown type \n";
+					}
+					else {
+						// echo $entry . ": " . $blowup[ count( $blowup ) - 3 ] . "\n";
+					}
+				}
 			}
-			else {
-				echo $entry . ": " . $blowup[ count( $blowup ) - 3 ] . "\n";
-			}
+
 		}
 	}
 
-	public static function getSongContents( $id ){
+	public static function getSongContents( $song ){
 		$mainDir = getcwd() . "/public";
-		$songDir = $mainDir . "/" . Songs::getUrl( $id );
-		$contents = scandir( $songDir );
-		for($index = count( $contents ) - 1; $index >= 0; $index-- ){
-			if( is_dir( $mainDir . "/" . $contents[ $index ] )){
-				unset( $contents[ $index ] );
+		$url = Songs::getUrl( $song );
+		if( $url == null ){
+			return null;
+		} else {
+			$songDir = $mainDir . "/" . Songs::getUrl( $song );
+			echo "SONG (" . $song->id . "): " . $songDir . "\n";
+			$contents = scandir( $songDir );
+			for($index = count( $contents ) - 1; $index >= 0; $index-- ){
+				if( is_dir( $mainDir . "/" . $contents[ $index ] )){
+					unset( $contents[ $index ] );
+				}
 			}
+			return $contents;
 		}
-		return $contents;
 	}
 
-	public static function getUrl( $id ){
-		$directory = Songs::getSongDirectory( Songs::lookupPrefix( $id ));
+	public static function getUrl( $song ){
+		$directory = Songs::getSongDirectory( Songs::lookupPrefix( $song ));
 		if( $directory != null ){
 			return Songs::$main_song_directory . "/" . $directory;
 		}
@@ -43,8 +61,8 @@ class Songs extends Eloquent {
 		}
 	}
 
-	public static function lookupPrefix( $id ){
-        $song = Songs::find( $id );
+	public static function lookupPrefix( $song ){
+        // $song = Songs::find( $id );
         if( isset( $song ) ){
         	$prefix = null;
         	if( isset( $song->vocalprefix ) && !empty( $song->vocalprefix ) ){
