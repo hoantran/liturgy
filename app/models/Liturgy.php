9e683501ac0344b6e6ecec7cdea4cbf240dc43ce
hoan <hoan@bluepego.com>
2013-11-04 18:01:00 -0800

    Added Liturgy::duplicateLiturgy()

--- app/models/Liturgy.php
+++ app/models/Liturgy.php
@@ -37,12 +37,46 @@ class Liturgy extends Eloquent {
 			if( !array_key_exists( $section, $master )){
 				$master[ $section ] = array();
 			}
-			$master[ $section ][ $p->part ] = $p->pivot->song_id;
+ 			// $master[ $section ][ $p->part ] = $p->pivot->song_id;
+ 			$master[ $section ][] = array(
+ 				'order'		=> $p->part_order,
+ 				$p->part 	=> $p->pivot->song_id
+ 			);
 		}
+
+		$master = Liturgy::sortParts( $master );
 
 		Log::info( "unpack:", $master );
 
 		return Liturgy::getFormattedSections( $master );
+	}
+
+	public static function sortParts( $sections ){
+		$results = array();
+		ksort( $sections ); // sections are ordered alphabettically by their names
+		foreach ($sections as $sectionName => $parts ) {
+			$items = array();
+
+			// sort the parts according to its order
+			$parts = array_values( array_sort( $parts, function( $value ){
+				return $value[ 'order' ];
+			}));
+
+			foreach ($parts as $p ) {
+				// each part - $p - should have only two elements: 
+				// 'order' and
+				// 'whatever the part name is' e.g. Processional, Kyrie ... etc
+				foreach ($p as $key => $value) {
+					if ( 0 != strcmp( $key, 'order' ) ) {
+						$items[ $key ] = $value;
+						break;
+					}
+				}
+			}
+			$results[ $sectionName ] = $items;
+		}
+
+		return $results;
 	}
 
 	public static function getFormattedSections( $sections ){
@@ -69,7 +103,37 @@ class Liturgy extends Eloquent {
 		Log::info( "FORMATTED:", $results );
 		return $results;
 	}
+
+	public static function duplicateLiturgy( $id ){
+		$src = Liturgy::find( $id );
+		$dst = $src->replicate();
+		$dst->title = Liturgy::getNewLiturgyTitle( $src->title );
+		$dst->save();
+		$dst->parts = $src->parts;
+		foreach( $dst->parts as $p ){
+			$p->pivot->liturgy_id = $dst->id;
+			$dst->parts()->attach( $dst->id, $p->pivot->toArray() );
+		}
+	}
+
+	public static function getNewLiturgyTitle( $srcTitle ){
+		for( $count = 1; $count < 10; $count++ ){
+			$newTitle = $srcTitle . '(' . $count . ')';
+			$copies = Liturgy::where('title', '=', $newTitle )->count();
+			if( 0 == $copies ){
+				return $newTitle;
+			}
+		}
+		return $srcTitle . '(' . 'LIMIT EXCEEDED' . ')';
+	}
 }
+
+// $one = Liturgy::find( 1 );
+// $two = Liturgy::find( 2 );
+// $p = $one->parts[ 0 ];
+// $p->pivot->liturgy_id=2;
+// $two->parts()->attach( 2, $p->pivot->toArray() );
+
 
 
 // $l = Liturgy::find( 1 );
