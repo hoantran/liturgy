5c1b0a5f84e03b4449e85f340fc2c0e35b8f22bc
hoan <hoan@bluepego.com>
2014-04-28 23:58:20 -0700

    For FLOCK

--- public/js/app/entities/Choir.js
+++ public/js/app/entities/Choir.js
@@ -9,26 +9,26 @@ define(["App"], function(App){
 
         // hardcoded for Faith choir:
         // TODO: retrieve it from the server
-        defaults: {
-            "name"          : "Faith Choir",
-            "nameBold"      : "FAITH CHOIR",
-            "slogan"        : "Sing for Fun! Eat for Real!",
-            "headline"      : "Ever Faithful",
-            "jumboUrl"      : "../img/featured.jpg",
-            "parishUrl"     : "http://www.stelizabethmilpitas.org",
-            "serving"       : "Serving St. Elizabeth Parish 9:30 AM Sunday Community, Milpitas, CA"
-        },
+        // defaults: {
+        //     "name"          : "Faith Choir",
+        //     "nameBold"      : "FAITH CHOIR",
+        //     "slogan"        : "Sing for Fun! Eat for Real!",
+        //     "headline"      : "Ever Faithful",
+        //     "jumboUrl"      : "../img/featured.jpg",
+        //     "parishUrl"     : "http://www.stelizabethmilpitas.org",
+        //     "serving"       : "Serving St. Elizabeth Parish 9:30 AM Sunday Community, Milpitas, CA"
+        // },
         // hardcoded for FLOCK choir:
         // TODO: retrieve it from the server
-        // defaults: {
-        //     "name"          : "FLOCK Choir",
-        //     "nameBold"      : "FLOCK CHOIR",
-        //     "slogan"        : "We'll sing for food!",
-        //     "headline"      : "One Voice!",
-        //     "jumboUrl"      : "../img/flock.jpg",
-        //     "parishUrl"     : "http://www.stpatrickvn.org",
-        //     "serving"       : "Serving Our Lady of Lavang Parish 8:30 PM Youth Mass Community, San Jose, CA"
-        // },
+        defaults: {
+            "name"          : "FLOCK Choir",
+            "nameBold"      : "FLOCK CHOIR",
+            "slogan"        : "We'll sing for food!",
+            "headline"      : "One Voice!",
+            "jumboUrl"      : "../img/flock.jpg",
+            "parishUrl"     : "http://www.stpatrickvn.org",
+            "serving"       : "Serving Our Lady of Lavang Parish 8:30 PM Youth Mass Community, San Jose, CA"
+        },
 
         // Get's called automatically by Backbone when the set and/or save methods are called (Add your own logic)
         validate: function(attrs) {
