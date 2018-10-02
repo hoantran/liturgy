d1addd7163064d4a396b005c0399d5689b11a90a
huygiang1393 <huygiang1393@gmail.com>
2018-10-01 20:40:05 -0700

    added composer to fs

--- tools/firestore-upload/json-to-firestore.js
+++ tools/firestore-upload/json-to-firestore.js
@@ -3,22 +3,25 @@
 //setup firebase admin sdk
 const admin = require('./node_modules/firebase-admin');
 const serviceAccount = require("./service-key.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    // databaseURL: "https://project-liturgy.firebaseio.com"
-});
 
 //loading json converted sql files
 const songs= require("./sql_json_dump/worship.songs.json");
 const composer_song = require("./sql_json_dump/worship.composer_song.json");
 const media_list = require("./sql_json_dump/worship.media.json");
+const composers = require("./sql_json_dump/worship.composer.json");     //huy
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+     databaseURL: "https://liturgy-407e8.firebaseio.com"
+});
+
 
 
 //create arrays for composers and media
 songs.forEach(function(element){
-    element.composers = []
-    element.medias = []
-    element.tags = []
+    element.composers = [];
+    element.medias = [];
+    element.tags = [];
 });
 
 //add composers to each song
@@ -28,7 +31,7 @@ composer_song.forEach(function(element){
 
 
 media_list.forEach(function(element){
-    songs.find(song => song.id == element.song_id).medias.push(element.file_name)
+    songs.find(song => song.id == element.song_id).medias.push(element.file_name);
 });
 
 //write songs collection to FireStore
@@ -40,4 +43,15 @@ songs.forEach(function(element){
                 .catch((error) => {
                     console.error("Error writing document: ", error);
                 });
-}); 
+});
+
+//write composers collection to Firestore
+composers.forEach(function(element){
+    admin.firestore().collection("composers").add(element)
+        .then((res) => {
+                    console.log("Document successfully written!");
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+});
