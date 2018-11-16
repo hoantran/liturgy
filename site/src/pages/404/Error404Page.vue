c5b54a46535045bd85c8ccf1dc31a0d4ef4be4ba
huygiang93 <huygiang1393@gmail.com>
2018-11-15 23:07:01 -0800

    added 404 page

---
+++ site/src/pages/404/Error404Page.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container col-lg-2 col-md-3 col-sm-4 col-xs-6 " style="text-align:center" >
+     <img :src="imageFile" alt="Page Not Found" height="400" width="400">
+    <h1>Sorry! The page you're looking for doesn't exist..</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Error404Page',
+  data () {
+    return {
+      imageFile: ''
+    }
+  },
+  created () {
+    // this.imageFile = '../../../static/404_2.jpg'
+    var index = Math.floor(Math.random() * 4)
+    // var images = require.context('../../static/', /\.jpg$/)
+    this.imageFile = '../../../static/404_' + index + '.jpg'
+  }
+}
+</script>
+<style scoped>
+</style>
