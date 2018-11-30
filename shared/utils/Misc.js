50c4de9fef28492dcd20e709e6399e7e7451fd04
hoantran <hoan@bluepego.com>
2018-11-30 00:41:45 -0800

    Fixed song media upload from server and updating download URL from admin client so that they would conform to the latest schema. Made sure media type is determined at one place (utils/Misc.js)

---
+++ shared/utils/Misc.js
@@ -0,0 +1,17 @@
+
+// https://fullstack-developer.academy/unexpected-token-export-error/
+// https://stackoverflow.com/questions/3225251/how-can-i-share-code-between-node-js-and-the-browser
+// https://caolan.org/posts/writing_for_node_and_the_browser.html
+const utils =  {
+  defaultMediaType: 'unknown',
+  supportedMediaType: ['guitar', 'piano', 'vocal', 'mp3', 'link', 'solo', 'octavo', 'lead', 'unknown'],
+  mediumType(type) {
+    if (this.supportedMediaType.includes(type)) {
+      return type
+    } else {
+      return this.defaultMediaType
+    }
+  }
+}
+
+module.exports = utils
--- site/src/pages/updatesongurls/UpdateSongURLsPage.vue
+++ site/src/pages/updatesongurls/UpdateSongURLsPage.vue
@@ -2,49 +2,13 @@
   <section class="container">
     <div class="uploadsongurls">
         <h1 class="title is-4">Update Song URLs to Download URLs</h1>
-        <p v-for="(text, index) in area_texts" :key="index"> {{ text }} </p>
-
-        <!-- <textarea class="status" name="status"
-                  rows="20" readonly
-                  wrap="hard">
-        </textarea>
-         -->
+        <p class="log-msg" v-for="(text, index) in area_texts" :key="index"> {{ text }} </p>
     </div>
   </section>
 </template>
 
 <script>
 import { songsCollection, storage } from '../../firebase/FirebaseInit'
-
-// const pathReference = storage.ref('ps 126 the lord has done/ps 126 the lord has done great things for us.vocal.1.pdf')
-// console.log('PDF', pathReference)
-// pathReference.getDownloadURL().then((url) => {
-//   console.log('URL', url)
-// }).catch((error) => {
-//   // A full list of error codes is available at
-//   // https://firebase.google.com/docs/storage/web/handle-errors
-//   switch (error.code) {
-//     case 'storage/object-not-found':
-//       // File doesn't exist
-//       console.log('storage/object-not-found')
-//       break
-
-//     case 'storage/unauthorized':
-//       // User doesn't have permission to access the object
-//       console.log('storage/unauthorized')
-//       break
-
-//     case 'storage/canceled':
-//       // User canceled the upload
-//       console.log('storage/canceled')
-//       break
-
-//     case 'storage/unknown':
-//       // Unknown error occurred, inspect the server response
-//       console.log('storage/unknown')
-//       break
-//   }
-// })
 
 export default {
   name: 'UpdateSongURLsPage',
@@ -54,33 +18,14 @@ export default {
     }
   },
   created () {
-    this.area_texts.push('Getting a song list ...')
-    // this.getX()
+    // console.log(utils.mediumType('vocal'))
     try {
-      this.getSongs()
+      this.update()
     } catch (err) {
       console.log('Error getting documents', err)
     }
   },
   methods: {
-    getX () {
-      songsCollection
-        .get()
-        .then(docs => {
-          if (docs.size === 0) {
-            console.log('server came back empty.')
-          } else {
-            docs.forEach(doc => {
-              let myID = doc.id
-              console.log('got my ID: ' + myID)
-            })
-          }
-        })
-        .catch(function (error) {
-          console.error(error)
-          console.log('Can NOT acquire myID')
-        })
-    },
     isLink (url) {
       const httpPattern = /^https?:\/\//
       if (httpPattern.test(url)) {
@@ -89,67 +34,56 @@ export default {
         return false
       }
     },
-    async getSongs () {
+    async update () {
       try {
         let songsRef = await songsCollection.get()
         let docs = songsRef.docs
         docs.forEach(async songDoc => {
           let song = songDoc.data()
-          let status = 'UNDETECTED '
-          // let status = 'UPDATED    '
-          // let status = 'DONE       '
-          let paths = song.urls
-          if (paths) {
-            let urls = []
-            let msgs = []
-            for (let urlProperty of paths) {
-              if (this.isLink(urlProperty)) {
-                status = 'DONE       '
-                urls.push(urlProperty)
-              } else {
+          let title = song.title
+          if (!title) {
+            title = 'NO TITLE'
+          }
+          title = title.slice(0, 31).padEnd(31, '.')
+          let media = song.media
+          let dirty = false
+          if (media) {
+            for (const [index, medium] of media.entries()) {
+              let url = medium.url
+              if (url && !this.isLink(url)) {
                 try {
-                  const pathReference = storage.ref(urlProperty)
-                  let url = await pathReference.getDownloadURL()
-                  urls.push(url)
-                  // console.log(url)
-                  // await songDoc.ref.update({url: url})
-                  status = 'UPDATED    '
-                  // [1] https://firebase.google.com/docs/reference/js/firebase.firestore.QueryDocumentSnapshot
-                  // [2] https://cloud.google.com/nodejs/docs/reference/firestore/0.17.x/QueryDocumentSnapshot#ref
-                  // why [1] is incomplete, not mentioning about ref and other attributes of QueryDocumentSnapshot ?
-                  // console.log('REF: ', songDoc.ref)
-                  // console.log('PATH: ', songDoc.ref.path)
-                } catch (urlError) {
-                  status = 'UNDETECTED '
-                  urls.push(urlProperty)
+                  const pathReference = storage.ref(url)
+                  let newURL = await pathReference.getDownloadURL()
+                  if (newURL) {
+                    media[index].url = newURL
+                    dirty = true
+                    this.area_texts.push('---   ' + `[${title}][QUEUED UP] [${medium.type.padEnd(7, '.')}] ${newURL.slice(70, 145)}`)
+                  }
+                } catch (error) {
+                  //   // A full list of error codes is available at
+                  //   // https://firebase.google.com/docs/storage/web/handle-errors
+                  this.area_texts.push('---   ' + `[${title}][ CAN NOT GENERATE URL ] [${error.code.slice(0, 25)}] ${url.slice(71, 115)}`)
                 }
               }
-              // this.area_texts.push('---' + `   [${status}] ${song.title} [${urlProperty}]`)
-              msgs.push('---' + `   [${status}] ${song.title} [...${urlProperty.slice(-50, -1)}]`)
             }
-            await songDoc.ref.update({urls: urls})
-            // Array.prototype.push.apply(this.area_texts, msgs)
-            for (let msg of msgs) {
-              this.area_texts.push(msg)
+            if (dirty) {
+              // [1] https://firebase.google.com/docs/reference/js/firebase.firestore.QueryDocumentSnapshot
+              // [2] https://cloud.google.com/nodejs/docs/reference/firestore/0.17.x/QueryDocumentSnapshot#ref
+              // why [1] is incomplete, not mentioning about ref and other attributes of QueryDocumentSnapshot ?
+              // console.log('REF: ', songDoc.ref)
+              // console.log('PATH: ', songDoc.ref.path)
+              await songDoc.ref.update({media: media})
+              this.area_texts.push('---   ' + `[${title}][UPDATED .]`)
+            } else {
+              this.area_texts.push('---   ' + `[${title}][NOTHING TO UPDATE]`)
             }
           } else {
-            this.area_texts.push('---' + `   [${status}] ${song.title} [${paths}]`)
+            this.area_texts.push('---' + `   [${title}][NO MEDIA ]`)
           }
         })
-        this.area_texts.push('END')
       } catch (error) {
         console.error('Encountered problem while updating song URLs', error)
       }
-      // for (let doc in songsRef.docs) {
-      //   console.log(doc)
-      // }
-      // for (QueryDocumentSnapshot of songsRef.docs) {
-      //   console.log(song)
-      // //   // let tasksRef = await campaignsRef.doc(campaign.id).collection('tasks').get()
-      // //   // for(task of tasksRef.docs) {
-      // //   //     console.log(task.id, task.data())
-      // //   // }
-      // }
     }
   }
 
@@ -171,5 +105,8 @@ export default {
   box-sizing: border-box;
   font-family: 'Arial', Arial, sans-serif;
   font-size: 18px;
+}
+.log-msg {
+  font-family: monospace;
 }
 </style>
