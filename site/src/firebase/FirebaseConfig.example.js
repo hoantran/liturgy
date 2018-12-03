9182332162e9da821c1006c2923c3c84172891ff
hoantran <hoan@bluepego.com>
2018-12-02 17:42:30 -0800

    Sample firebase config file for web client

---
+++ site/src/firebase/FirebaseConfig.example.js
@@ -0,0 +1,10 @@
+// The info here is from firebase-console > project setting > "Add Firebase to your web app"
+
+export default {
+  apiKey: '39-char-string',
+  authDomain: 'your-firebase-project.firebaseapp.com',
+  databaseURL: 'https://your-firebase-project.firebaseio.com',
+  projectId: 'your-firebase-project',
+  storageBucket: 'your-firebase-project.appspot.com',
+  messagingSenderId: 'decimal-number-11-digit-long'
+}
