0c545b887f9601f108efe7f29656adc784958c5d
bnguyen312 <btnguye@ucdavis.edu>
2020-05-09 12:08:11 -0700

    FW-4 max fuzzy matching, separate vocal and instrumental pdf creation, code cleanup

--- mergePDF/search-n-merge.py
+++ mergePDF/search-n-merge.py
@@ -1,39 +1,18 @@
+import fnmatch
 import gspread
+import os
+import PyPDF2
+from fuzzywuzzy import process
 from oauth2client.service_account import ServiceAccountCredentials
-from fuzzywuzzy import fuzz, process
-from dateutil.parser import parse
-import pandas as pd
-import numpy as np
-import os, glob
-from PyPDF2 import PdfFileMerger
 
-song_directory = ""
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
-
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("FLOCK Song List").sheet1
-
-
-# date = sheet.find(input('Enter date(M/D/YYYY): '))
+#Set Sunday date below and set path to your local song directory
 date = '5/10/2020'
-# TODO: can we improve by allowing different/multiple date formats?
-# date = parse(input('Enter date: '), fuzzy=True)
-# print(sheet.col_values(1),sheet.col_values(date.col))
-
-# format_frame = pd.DataFrame(sheet.col_values(1))
-# songs = pd.DataFrame(sheet.col_values(date.col))
-
-
-def remove_file_extension(filename):
-    return os.path.splitext(filename)[0]
+song_directory = ""
 
 def find_vocal_pdf(directory):
     '''
-    Function to find a filename with 'vocal' as a substring. If only one file
-    in directory, that file is returned regardless if it contains 'vocal'.
+    Function to find a filename with 'vocal' as a substring. Will assert if no
+    pdfs are found in directory.
 
     Parameters
         directory (str) - absolute path to directory to be searched
@@ -42,7 +21,7 @@ def find_vocal_pdf(directory):
         filename (str) - filename of vocal PDF
     '''
 
-    files = os.listdir(directory)
+    files = fnmatch.filter(os.listdir(directory), "*.pdf")
     assert(len(files) != 0)
     if(len(files) == 1):
         return files[0]
@@ -50,6 +29,38 @@ def find_vocal_pdf(directory):
         for file in files:
             if("vocal" in file):
                 return file
+        for file in files:
+            if("guitar" in file):
+                return file
+        return files[0]
+
+def find_instrumental_pdf(directory):
+    '''
+    Function to find a filename with 'guitar' or 'piano' as a substring. Will assert if no
+    pdfs are found in directory.
+
+    Parameters
+        directory (str) - absolute path to directory to be searched
+
+    Returns
+        filename (str) - filename of vocal PDF
+    '''
+
+    files = fnmatch.filter(os.listdir(directory), "*.pdf")
+    assert(len(files) != 0)
+    if(len(files) == 1):
+        return files[0]
+    else:
+        for file in files:
+            if("guitar" in file):
+                return file
+        for file in files:
+            if("piano" in file):
+                return file
+        for file in files:
+            if("vocal" in file):
+                return file
+        return files[0]
         
 def handle_matching_error(song):
     '''
@@ -71,32 +82,54 @@ def handle_matching_error(song):
     return filepath
 
 if song_directory == "":
-    print("Error please set song_directory filepath to your local song directory.")
+    print("Error please set song_directory filepath variable in this P to your local song directory.")
     assert()
 
+#Pull data from Google Sheet
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("FLOCK Song List").sheet1
 cells = sheet.col_values(sheet.find(date).col)[2:] # remove first 2 cells which are Sunday and Date
 songs = list(filter(None, cells)) #filter out empty cells
-filepaths=[] #list of filepaths for pdfs to merge
 
-#finding pdf match for each song
+#Search song directory pdfs for each song
+vocal_set=[]
+instrumental_set=[]
+dirs = os.listdir(song_directory)
+
 for song in songs:
-    found_pdf = False
-    for dir in os.listdir(song_directory):
-        if fuzz.token_set_ratio(dir, song)>80: # i think score of 80 is reliable. dont ask me why
-            directory_path = os.path.join(song_directory, dir)
-            filename = find_vocal_pdf(directory_path)
-            filepath = os.path.join(directory_path, filename)
-            filepaths.append(filepath)
-            found_pdf = True
-            break
-    #Matching PDF for song could not be found
-    if(not found_pdf):
-        filepaths.append(handle_matching_error(song))
+    directory_name, match_score = process.extractOne(song, dirs)
+    if match_score > 80:
+        directory_path = os.path.join(song_directory, directory_name)
 
-#creating pdf
+        vocal_filename = find_vocal_pdf(directory_path)
+        print("For [" + song + "] found a " + str(match_score) + "% matching directory ["
+         + directory_name +  "] and vocal file [" + vocal_filename + "]")
+        filepath = os.path.join(directory_path, vocal_filename)
+        vocal_set.append(filepath)
+
+        instrumental_filename = find_instrumental_pdf(directory_path)
+        print("For [" + song + "] found a " + str(match_score) + "% matching directory ["
+         + directory_name +  "] and instrumental file [" + instrumental_filename + "]")
+        filepath = os.path.join(directory_path, instrumental_filename)
+        instrumental_set.append(filepath)
+    
+    else:
+        print("pdf not found for song: " + song)
+        #vocal_set.append(handle_matching_error(song)) #uncomment to request missing pdf from user
+
+#Create pdf sets
 filename = "FLOCK_" + date.replace("/", "_") + "_vocal.pdf"
-merger = PdfFileMerger(strict=False)
-for filepath in filepaths:
+merger = PyPDF2.PdfFileMerger(strict=False)
+for filepath in vocal_set:
     merger.append(filepath)
 merger.write("./"+filename)
+
+filename = "FLOCK_" + date.replace("/", "_") + "_instrumental.pdf"
+merger = PyPDF2.PdfFileMerger(strict=False)
+for filepath in instrumental_set:
+    merger.append(filepath)
+merger.write("./"+filename)
+
 merger.close()
